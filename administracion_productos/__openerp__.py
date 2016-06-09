86d098308d72991a7d4ada8586c154d14dc9fa10
maritza <maritzaesparza05@gmail.com>
2016-06-08 19:08:44 -0500

    Borrado módulo de administración

--- administracion_productos/__openerp__.py
+++
@@ -1,8 +0,0 @@
-{
-'name': 'Administration Product',
-'description': 'Administrador Productos',
-'author': 'Maritza Esparza',
-'depends': ['mail'],
-'application': True,
-'data':['admin_product_view.xml'],
-}
